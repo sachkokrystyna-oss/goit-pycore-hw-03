8b93430e4ca81aad91b522af5da529a13d337d73
sachkokrystyna-oss <sachkokrystyna@gmail.com>
2026-04-26 16:47:18 +0300

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def get_days_from_today(date: str) -> int | None:
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        return (today - input_date).days
+    except ValueError:
+        return None
+
+print(get_days_from_today("2020-10-09"))
